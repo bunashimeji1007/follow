3bfc4a3ba5975018dec87f5f97d22eca119933ea
bunashimeji1007 <bunashimeji1007m@gmail.com>
2020-06-22 16:53:33 +0900

    first commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,6 +10,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @user = @book.user
     @newbook = Book.new
+    @comment = Comment.new
   end
 
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#top'
   get 'home/about'
-  resources :books
+  resources :books do
+  	  resources :comments, only: [:create, :destroy]
+  	  resource :favorites, only: [:create, :destroy]
+  end
   resources :users,only: [:show,:index,:edit,:update]
 end
